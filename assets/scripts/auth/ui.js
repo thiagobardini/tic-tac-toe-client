d696a867786ab341e17ce78f1887162a3a8745d1
thiagobadini <thiagobardini@hotmail.com>
2021-02-28 20:11:13 -0500

    The game is ready for review

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -62,6 +62,7 @@ const changePasswordFailure = function (response) {
 const signOutSuccess = function () {
   // hide events
   $('#sign-out').hide()
+  $("#viewGameBoard").hide();
 
   $('#change-password').hide()
   // show sign-in and sign-up
